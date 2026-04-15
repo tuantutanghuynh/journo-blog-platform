53ed83c0726878f28576d4ab2227481b370b0f21
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-15 22:08:05 +0700

    write category model

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'color',
+        'parent_id',
+    ];
+
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
