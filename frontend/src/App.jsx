0ee784f87fde6eacc80180dec3f4c059568a4d4d
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-30 20:21:30 +0700

    feat: add React frontend with Home, Login, Register pages and Axios setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import "./App.css";
+import Register from "./pages/Register";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
