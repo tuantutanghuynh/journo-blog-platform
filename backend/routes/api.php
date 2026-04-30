1535084cf6a4affd112a1ff171a0444579455a70
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-30 10:45:05 +0700

    updated api routes added some new routes for comments

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,17 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CommentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Public routs
+//Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/posts',      [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
-
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::get('/categories/{id}/posts', [CategoryController::class, 'posts']);
+Route::get('/posts/{postId}/comments', [CommentController::class, 'index']);
 
 //protected routes - must have token to enter
 Route::middleware('auth:sanctum')->group(function () {
@@ -23,4 +28,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/posts',           [PostController::class, 'store']);
     Route::put('/posts/{id}',       [PostController::class, 'update']);
     Route::delete('/posts/{id}',    [PostController::class, 'destroy']);
+    Route::post('/posts/{postId}/comments', [CommentController::class, 'store']);
+    Route::delete('/comment/{id}', [CommentController::class, 'destroy']);
 });
